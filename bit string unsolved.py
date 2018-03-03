f7832fb5261b535bfdc6a95d08f11bfcf9706d11
Aman Agarwal <agaraman0@gmail.com>
2018-03-04 03:01:45 +0530

    Add files via upload

---
+++ bit string unsolved.py
@@ -0,0 +1,8 @@
+def appendAtBcginningFront(x, L):
+return Ix + clcmunt for clement in LI
+clef bitStrings(n):
+if n == 0: return 11
+Lr n == 1: return 1"0", "1 "J
+else:
+return (appcndAtBcginningFront("O", bitStrings(n-1)) + appcndAtBeginningFront(" l", bitStrings(n-1 )))
+print bitStrings(4)
